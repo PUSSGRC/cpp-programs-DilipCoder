02f5c4ec29732117b01567f687b4417f24864ec4
dilip-cse <krdilip276@gmail.com>
2017-05-29 13:25:34 +0530

    get_pus function demonstration

---
+++ get_put/get_put.cpp
@@ -0,0 +1,13 @@
+#include<iostream> //header file for standerd input output stream.
+	using namespace std; //binding the standered function.
+ /*****************************************
+ * get and put function takes/gives input *
+ * and output as a character.             *
+ * ****************************************/
+int main()
+{
+	char text;
+	cin.get(text);
+	cout.put(text);
+	return 0;
+}
